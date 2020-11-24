63b1ab9ac20ab67c173c8425c7d36195bff9a5c2
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2020-11-23 22:56:41 -0500

    Configuring Text Styles for Home Screen.

---
+++ lib/config/style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class TextStyles {
+
+  static const TextStyle blackText = const TextStyle(
+      color: Colors.black,
+  );
+
+  static const TextStyle blackBoldText = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle greyText = const TextStyle(
+    color: Colors.grey,
+  );
+
+  static const TextStyle greenText = const TextStyle(
+    color: Colors.green,
+  );
+}
--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stonks_app/config/style.dart';
 import 'package:stonks_app/home/widgets/customAppBar.dart';
 import 'package:stonks_app/home/widgets/customBottomNavBar.dart';
 
@@ -7,17 +8,74 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      body: getHomeData(),
       bottomNavigationBar: CustomBottomNavBar(),
+    );
+  }
+
+  getHomeData() {
+    return ListView(
+      children: <Widget>[
+        _walletPosition(),
+        // _graphData(),
+        _buyingPower()
+      ],
+    );
+  }
+
+  _walletPosition() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 12.0, top: 12.0, bottom: 12.0, right: 22.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text('TODAY', style: TextStyles.greyText,),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.arrow_upward, color: Colors.green,),
+                  Text('2.62%', style: TextStyles.greenText, )
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text('TOTAL %', style: TextStyles.greyText,),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.arrow_upward, color: Colors.green,),
+                  Text('32.14%', style: TextStyles.greenText, )
+                ],
+              )
+            ],
+          ),Column(
+            children: <Widget>[
+              Text('TOTAL \$', style: TextStyles.greyText,),
+              Text('\$330,961%', style: TextStyles.blackBoldText, )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _buyingPower() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text('Buying Power', style: TextStyles.blackText,),
+          Row(
+            children: <Widget>[
+              Text('\$300.03', style: TextStyles.blackText,),
+              Icon(Icons.keyboard_arrow_right),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
