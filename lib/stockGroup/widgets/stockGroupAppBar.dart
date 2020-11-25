39371e8b627d1417fc198f76f3355038b617cbb4
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2020-11-25 10:09:54 -0500

    Adjusting stock group App Bar layout.

--- lib/stockGroup/widgets/stockGroupAppBar.dart
+++ lib/stockGroup/widgets/stockGroupAppBar.dart
@@ -27,45 +27,50 @@ class StockGroupAppBar extends StatelessWidget implements PreferredSize {
   }
 
   _buildStockInfoFields() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Text(stock.name, style: TextStyles.blackBoldText,textScaleFactor: 0.8,),
-                Icon(Icons.info_outline, size: 18,)
-              ],
-            ),
-            Text('${stock.members} members', style: TextStyles.greyText, textScaleFactor: 0.7,)
-          ],
-        ),
-        Column(
-          children: <Widget>[
-            Text('\$${stock.stockValue}', style: TextStyles.blackBoldText, textScaleFactor: 0.8,),
-            Container(
-              child: Row(
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Icon((stock.variation>0) ? Icons.arrow_upward : Icons.arrow_downward, color: (stock.variation>0) ? Colors.green : Colors.redAccent, size: 18,),
-                  Text('${stock.variation}%', style: (stock.variation>0) ? TextStyles.greenText : TextStyles.redText, textScaleFactor: 0.8, )
+                  Text(stock.name, style: TextStyles.blackBoldText,textScaleFactor: 0.8,),
+                  Icon(Icons.info_outline, size: 18,)
                 ],
               ),
-            ),
-          ],
-        )
-      ],
+              Text('${stock.members} members', style: TextStyles.greyText, textScaleFactor: 0.7,)
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text('\$${stock.stockValue}', style: TextStyles.blackBoldText, textScaleFactor: 0.8,),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Icon((stock.variation>0) ? Icons.arrow_upward : Icons.arrow_downward, color: (stock.variation>0) ? Colors.green : Colors.redAccent, size: 18,),
+                    Text('${stock.variation}%', style: (stock.variation>0) ? TextStyles.greenText : TextStyles.redText, textScaleFactor: 0.8, )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );  
   }
 
   _buildTradeButton() {
-    return FlatButton(
-        onPressed: () {},
-        child: Text('Trade', style: TextStyles.whiteText,),
-        color: Colors.green,
-        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+    return Padding(
+      padding: const EdgeInsets.only(left: 4.0),
+      child: FlatButton(
+          onPressed: () {},
+          child: Text('Trade', style: TextStyles.whiteText,),
+          color: Colors.green,
+          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+      ),
     );
   }
 
